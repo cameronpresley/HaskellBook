fb3b549d1f7c70e01d7e09d6d413a226daa14a96
Cameron Presley <Cameron@TheSoftwareMentor.com>
2018-06-22 21:07:40 -0400

    Added exercise solutions for parantheses-and-associations

---
+++ chapter2/parantheses-and-associations.hs
@@ -0,0 +1,14 @@
+-- Question 1
+-- 8 + 7 * 9
+-- (8 + 7) * 9
+-- The two expression evaluate to be different
+
+-- Question 2
+-- perimeter x y = (x * 2) + (y * 2)
+-- perimeter x y = x * 2 + y * 2
+-- The two expressions evaluate to be the same
+
+-- Question 3
+-- f x = x / 2 + 9
+-- f x = x / (2 + 9)
+-- The two expressions evaluate to be different
