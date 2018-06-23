b6743b0feb6a9398f31b2c8c794e676db8a1490c
Cameron Presley <Cameron@TheSoftwareMentor.com>
2018-06-22 21:00:18 -0400

    Initial commit with first exercises of Chapter 2.

---
+++ chapter2/comprehension.hs
@@ -0,0 +1,12 @@
+-- Question 1
+half x = x / 2
+-- for repl -> let half x = x / 2
+
+square x = x * x
+-- for repl -> let square x = x * x
+
+-- Question 2
+area r = 3.14 * r * r
+
+-- Question 3
+area' r = pi * r * r
