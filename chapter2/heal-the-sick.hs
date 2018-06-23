303b66277448edfc579d394d0c5476de02cadee9
Cameron Presley <Cameron@TheSoftwareMentor.com>
2018-06-22 21:09:57 -0400

    Added exercises for heal-the-sick for chapter 2.

---
+++ chapter2/heal-the-sick.hs
@@ -0,0 +1,16 @@
+-- Question 1
+-- let area x = 3. 14 * (x * x)
+-- Should be: let area x = 3.14 * (x*x)
+
+-- Question 2
+-- let double x = b * 2
+-- Should be: let double x = x * 2
+
+-- Question 3
+-- x = 7
+--  y = 10
+-- f = x + y
+-- Should be:
+-- x = 7
+-- y = 10
+-- f = x + y
