1eb6c2c3c843be6017bdd5b12a67f72e7e81925d
Cameron Presley <Cameron@TheSoftwareMentor.com>
2018-06-24 20:43:59 -0400

    Added the find the mistakes exercise from chapter 4.

---
+++ chapter4/find-the-mistakes.hs
@@ -0,0 +1,16 @@
+-- Question 1
+-- not True && True
+
+-- Question 2
+-- not (x == 6)
+
+-- Question 3
+-- (1 * 2) == 5
+
+-- Question 4
+-- "Merry" > "Happy"
+
+-- Question 5
+-- No obvious fix, but the following would work
+-- "1, 2, 3" ++ "look at me!"
+-- ["1, 2, 3"] ++ ["look at me!"]
