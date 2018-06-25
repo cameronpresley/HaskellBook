b6dde44f2a4bcb2cf6b0ad5a7083ebba77e86fce
Cameron Presley <Cameron@TheSoftwareMentor.com>
2018-06-24 21:46:59 -0400

    Added parametricity exercises from chapter 5.

---
+++ chapter5/parametricity.hs
@@ -0,0 +1,15 @@
+-- Question 1
+-- ?? :: a -> a
+-- The only implementation is id
+-- id :: a -> a
+-- id a = a
+
+-- Question 2
+-- ?? :: a -> a -> a
+-- firstImplementation x y = x
+-- secondImplementation x y = y
+
+
+-- Question 3
+-- ?? :: a -> b -> b
+-- implementation a b = b
