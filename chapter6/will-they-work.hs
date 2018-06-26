c61107010ec36c49f36ac537ae5569eee3453829
Cameron Presley <Cameron@TheSoftwareMentor.com>
2018-06-26 12:11:19 -0400

    Added will they work exercise from chapter 6.

---
+++ chapter6/will-they-work.hs
@@ -0,0 +1,22 @@
+-- Question 1
+-- max (length [1, 2, 3]) (length [8, 9, 10, 11, 12])
+-- == max (length [1, 2, 3]) (5)
+-- == max (3) (5)
+-- == max 3 5
+-- == 5
+
+-- Question 2
+-- compare (3 * 4) (3 * 5)
+-- == compare (3*4) (15)
+-- == compare (12) (15)
+-- == LT
+
+-- Question 3
+-- compare "Julie" True
+-- This won't work because "Julie" and True are different types
+
+-- Question 4
+-- (5+3) > (3+6)
+-- == (5+3) > (9)
+-- == (8) > (9)
+-- == False
