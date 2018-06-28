9bea3bb7582eb2007d8c89eccb24d8c8fbf739f2
Cameron Presley <Cameron@TheSoftwareMentor.com>
2018-06-28 15:22:12 -0400

    Added enum from to exercise from chapter 9.

---
+++ chapter9/enum-from-to.hs
@@ -0,0 +1,11 @@
+eftBool :: Bool -> Bool -> [Bool]
+eftBool a b = enumFromTo a b
+
+eftOrd :: Ordering -> Ordering -> [Ordering]
+eftOrd a b = enumFromTo a b
+
+eftInt :: Int -> Int -> [Int]
+eftInt a b = enumFromTo a b
+
+eftChar :: Char -> Char -> [Char]
+eftChar a b = enumFromTo a b
